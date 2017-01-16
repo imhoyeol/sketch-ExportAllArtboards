c5bb63f91e0af2171e4909cb87cac937ba19cdd2
imhoyeol <imhoyeol@gmail.com>
2017-01-16 10:36:31 +0900

    Add files via upload
    주석 수정

--- ExportAllArtboards.sketchplugin/Contents/Sketch/ExportAllArtboards.js
+++ ExportAllArtboards.sketchplugin/Contents/Sketch/ExportAllArtboards.js
@@ -12,6 +12,8 @@ var onRun = function(context) {
 	//각 페이지를 하나씩 조회합니다.
 	for (var i = 0; i < allPages.count(); i++) {
 		var page = allPages[i];
+
+		//페이지 이름을 불러옵니다. 저장할 파일명에 사용됩니다.
 		var namePage = page.name();
 		
 		//각 페이지의 모든 아트보드를 조회합니다.
@@ -21,29 +23,31 @@ var onRun = function(context) {
 		for (var j = 0; j < allArtboards.count(); j++) {
 			var artboard = allArtboards[j]
 			
-			//아트보드의 이름을 조회합니다.
+			//아트보드의 이름을 조회합니다. 저장할 파일명에 사용됩니다.
 			var nameArtboard = artboard.name();
+
 			var fileNames = [];
 
+			//아트보드를 복제하고 Export 설정값을 초기화합니다.
 			var c = artboard.duplicate();
 			c.exportOptions().removeAllExportFormats();
 			var exportOption = c.exportOptions().addExportFormat();
 			exportOption.setScale(1);
 			exportOption.setName("@1x");
 
+			//저장할 경로와 파일명을 설정합니다.
 			var fileName= file_path + namePage + "/" + nameArtboard + ".png";
 			fileNames.push(fileName);
 
+			//Export 합니다.
 			var slices = MSExportRequest.exportRequestsFromExportableLayer(c);
-			
 			for (var k = 0; k < slices.count(); k++) {
 				[doc saveArtboardOrSlice:slices[k] toFile:fileNames[k]];
 			}
 			
+			//복제했던 아트보드를 삭제하고 레이어 선택을 해제합니다.
 			c.removeFromParent();
 			doc.currentPage().deselectAllLayers();
-
-			log(fileName= file_path + namePage + "/" + nameArtboard + ".png");
 		}
 	}
 }
